26b8cf7ab942545404da918777d878be0f0add33
rithaven <veneruth4@gmail.com>
2023-05-25 13:25:59 +0200

    Added all Tasks

---
+++ Task2.js
@@ -0,0 +1,22 @@
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+function solution(roman) {
+    // Transforming string roman numeral into an integer
+    const romanValues = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  
+    let decimalValue = 0;
+    for (let i = 0; i < roman.length; i++) {
+      // Get the decimal value of the current Roman numeral
+      const currentValue = romanValues[roman[i]];
+  
+      // Get the decimal value of the next Roman numeral (if exists)
+      const nextValue = romanValues[roman[i + 1]];
+  
+      // Add or subtract the current decimal value based on its comparison with the next value
+      decimalValue += currentValue < nextValue ? -currentValue : currentValue;
+    }
+    
+    // Return the calculated decimal value
+    return decimalValue;
+  }
---
+++ Task3.js
@@ -0,0 +1,37 @@
+// Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+function solution(number) {
+    // Convert the number to a Roman numeral
+    let numerals = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I",
+    ];
+    // Corresponding values for Roman numerals
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  
+    // Use reduce method to iterate over numerals array and build the Roman numeral representation
+    return numerals.reduce(function (result, numeral, index) {
+  
+      // Calculate the count of the current Roman numeral in the given number
+      let count = Math.floor(number / values[index]);
+  
+      // Update the number by taking the remainder after division
+      number %= values[index];
+  
+      // Accumulate the Roman numeral representation
+      return result + numeral.repeat(count);
+  
+    }, "");
+  }
