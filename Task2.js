a15d3c0929fcb1e0b20701837dd25fbe3c2d33e9
rithaven <veneruth4@gmail.com>
2023-05-25 17:54:12 +0200

    Modified functions and use arrow function

--- Task2.js
+++ Task2.js
@@ -2,7 +2,7 @@
 // Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
 
 function solution(roman) {
-    // Transforming string roman numeral into an integer
+   
     const romanValues = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
   
     let decimalValue = 0;
